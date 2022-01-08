b74f629a0ccfec4e3e670b8cdfc3d57e84a9baaf
Rounak Singh <65697913+singh-rounak@users.noreply.github.com>
2022-01-07 18:36:50 -0600

    Create singleNumber.py

---
+++ singleNumber.py
@@ -0,0 +1,24 @@
+
+class Solution(object):
+	def singleNumber(self, nums):
+		Hmap = {}
+
+		for num in nums:
+			if num not in Hmap:
+				Hmap[num]  = 1
+			else:
+				Hmap[num] += 1
+		for key in Hmap.keys():
+			if Hmap[key] ==1:
+				return key
+
+nums = [4,1,2,1,2]
+
+Object = Solution()
+print(Object.singleNumber(nums))
+
+"""
+In - [4,1,2,1,2]
+Out - 4
+[Finished in 0.2s]
+"""
