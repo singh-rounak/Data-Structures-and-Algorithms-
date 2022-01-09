fa27af06bdcbc44987025b2d43e501804657b6a0
Rounak <65697913+singh-rounak@users.noreply.github.com>
2022-01-09 11:29:57 -0600

    Create find_target.py

---
+++ Binary Search/find_target.py
@@ -0,0 +1,18 @@
+class solution:
+  def find_target(self, nums, target):
+    
+    for ind, num in enumerate(nums):
+        if num == target:
+            return ind
+    return -1
+    
+Object = Solution()
+print(Object.find_target([1,4,7,5,9,8,7,3,5], 9))
+
+"""
+OUT 4
+[Finished in 0.3s]
+
+Time: O(logn)
+Space:1
+"""
