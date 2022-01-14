b1c243d113295f424521fa9721c1402920347e8d
Rounak Singh <singhrounak1818@gmail.com>
2022-01-13 19:21:28 -0600

    Update Towers_of_Hanoi.py

--- Recursion/Towers_of_Hanoi.py
+++ Recursion/Towers_of_Hanoi.py
@@ -1,12 +1,13 @@
-def Hanoi(disk, source, middle, destination):
+def Hanoi(disk, source, M, destination):
+    # Base Case: if the smallest disk
     if disk == 1:
         print("disk %s from %s to %s" % (disk, source, destination))
         return
 
-    Hanoi(disk-1, source, destination, middle)
+    Hanoi(disk-1, source, destination, M)
     print("disk %s from %s to %s" % (disk, source, destination))
 
-    Hanoi(disk-1, middle, source, destination)
+    Hanoi(disk-1, M, source, destination)
 
 
 print(Hanoi(3, 'A', 'B', 'C'))
