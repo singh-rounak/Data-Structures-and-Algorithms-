86edd77ebc7d33187957e6e18fadf73bb71f1865
Rounak <65697913+singh-rounak@users.noreply.github.com>
2022-01-13 19:17:15 -0600

    Update Towers_of_Hanoi.py

--- Recursion/Towers_of_Hanoi.py
+++ Recursion/Towers_of_Hanoi.py
@@ -10,3 +10,14 @@ def Hanoi(disk, source, middle, destination):
 
 
 print(Hanoi(3, 'A', 'B', 'C'))
+
+"""
+Out - 
+disk 1 from A to C
+disk 2 from A to B
+disk 1 from C to B
+disk 3 from A to C
+disk 1 from B to A
+disk 2 from B to C
+disk 1 from A to C
+"""
