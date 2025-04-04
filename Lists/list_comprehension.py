3ec8ffaa5b61d42f6acca8eed97e93fe473ad7bf
Singh Rounak <rounaks@hexaware.com>
2025-04-04 13:32:01 +0530

    added syntax

--- Lists/list_comprehension.py
+++ Lists/list_comprehension.py
@@ -1,7 +1,48 @@
+'''
+LIST COMPREHESIONS IN PYTHON
+
+syntax : [expression for item in list if condition == True]
+
+'''
+
 nums = nums = [1,2,3,4,5,6,7,8,9,10]
 
-my_list = []
-for num in nums:
-  my_list.append(num)
+# my_list = []
+# for num in nums:
+#   my_list.append(num)
 
-print(my_list)
+# print(my_list)
+
+#List comprehension for creating a list with doubled numbers:
+a = [2 * num for num in nums]
+print('a = ', a)
+
+b = [2 + num for num in nums]
+print('b = ', b)
+
+new_list =[1,2,-3,4]
+doubled_positive_only = [num *2 for num in new_list if num > 0]
+
+print('new list = ',new_list)
+print('output = ' , doubled_positive_only)
+
+squared_numbers = [num*num for num in new_list]
+print('squared = ', squared_numbers)
+
+#Even numbers only
+
+even_only = [num for num in nums if num%2 == 0]
+print('even_only = ', even_only)
+
+#Range
+subset = [num for num in range(5)]
+print('subset=',subset)
+
+odd_subset = [num for num in range(5) if num %2 !=0]
+print("Odd subset = ", odd_subset)
+
+even_subset = [num for num in range(5,10) if num %2 ==0]
+print("Even subset = ", even_subset)
+
+for i in range(5,11):
+    print(i)
