b92eca5fbfa9f59600ea7115c8610906b281cf7e
Singh Rounak <rounaks@hexaware.com>
2025-04-09 14:00:10 +0530

    added ord

---
+++ Python Concepts/ord.py
@@ -0,0 +1,13 @@
+'''
+PYTHON  - ORD Function
+The 'ord' function of python returns an integer representation of the Unicode Character
+syntax - ord('character')
+
+the ord() function takes a single paramter only
+'''
+#Eg:
+#Find unicode of  'p':
+
+unicode_char = ord('p')
+print(unicode_char)
+
